25ed90564b85e22bfadf3f2d82d02e9ace537129
Madhavdheeraj <107113806+Madhavdheeraj@users.noreply.github.com>
2024-10-01 11:17:58 +0530

    Delete TaskFactory.java

--- TaskFactory.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalTime;
-
-public class TaskFactory {
-    public Task createTask(String description, String startTime, String endTime, String priority) {
-        LocalTime start = LocalTime.parse(startTime);
-        LocalTime end = LocalTime.parse(endTime);
-        return new Task(description, start, end, priority);
-    }
-}
